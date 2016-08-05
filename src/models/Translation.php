a19089134e8ea79900851c5f5dd6a4c195695f99
Artur Anyszek <a.anyszek@gmail.com>
2016-08-05 09:41:49 +0200

    initial

---
+++ src/models/Translation.php
@@ -0,0 +1,19 @@
+<?php
+
+namespace Bunta\Translations\Models;
+
+use Illuminate\Database\Eloquent\Model;
+
+class Translation extends Model {
+
+    public $timestamps = false;
+    protected $table = 'translations';
+
+    /**
+     * Attributes that should be mass-assignable.
+     *
+     * @var array
+     */
+    protected $fillable = ['table', 'foreign_id', 'lang', 'field', 'value'];
+
+}
